d01a1a77390fb27470ad6d7b0c29b70cc6c9cfd1
Fábio Veiga <veigabfabio@hotmail.com>
2025-02-06 17:46:58 -0300

    Ajustes

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,6 @@
 document.querySelector(".popup-close").addEventListener("click", ()=>{
     document.querySelector(".popup").remove()
+    document.querySelector(".popup-windowblock").remove()
     document.querySelector("header").style.filter = 'none'
     document.querySelector("main").style.filter = 'none'
     document.querySelector("footer").style.filter = 'none'
